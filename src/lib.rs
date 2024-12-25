7c018b8f9adabadc6caf8eef413e2110e709306c
⋆˚｡⋆୨ᴡɪᴛᴄʜQᴜᴇᴇɴ୧‧₊˚ <157842885+ksm-lunarflame@users.noreply.github.com>
2024-12-25 22:12:32 +0200

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
 pub mod Extensions;
 
-pub use Extensions::1_QQApplication::QQApplication;
-pub use Extensions::2_QQMainWindow::QQMainWindow;
+pub use Extensions::QQApplication::QQApplication;
+pub use Extensions::QQMainWindow::QQMainWindow;
